8ba1b59b530c19a46e5c607e2802fa83dc190dca
Nampeth <boss34052@live.com>
2021-11-24 14:46:10 +0700

    My first commit

--- server2.php
+++ server2.php
@@ -1,12 +1,17 @@
 <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "boss753142";
-    $dbname = "login"; 
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "boss753142";
+    // $dbname = "login"; 
+
+    $servername = "us-cdbr-east-04.cleardb.com";
+    $username = "b85f9d6e697478";
+    $password = "dcbeb1dc";
+    $dbname = "heroku_d048965f1125797"; 
 
     //Create Connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname, "3307");
+    $conn = mysqli_connect($servername, $username, $password, $dbname, "3306");
 
     //Check Connection
     if (!$conn) {
